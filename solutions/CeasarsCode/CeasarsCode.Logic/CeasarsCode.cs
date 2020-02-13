c64783d4d799f0dc2f0108eebaa582dc04582106
Maksym Dovhan <kerman901@gmail.com>
2020-02-13 18:54:20 +0200

    Add CeasarsCode solution

---
+++ solutions/CeasarsCode/CeasarsCode.Logic/CeasarsCode.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CeasarsCode.Logic
+{
+    public enum Alphabet
+    {
+        English,
+        Ukrainian
+    }
+
+    public static class CeasarsCode
+    {
+        private static Dictionary<Alphabet, string> alphabets;
+
+        static CeasarsCode()
+        {
+            alphabets = new Dictionary<Alphabet, string>
+            {
+                { Alphabet.English, " abcdefghijkmnopqrstuvwxyz" },
+                { Alphabet.Ukrainian, " абвгґдеєжзиіїйклмнопрстуфхцчшщьюя" }
+            };
+        }
+
+        public static bool IsKeyValid(int key, Alphabet alphabet)
+        {
+            return key < alphabets[alphabet].Length;
+        }
+
+        public static bool IsTextValid(string text, Alphabet alphabet)
+        {
+            foreach (var symbol in text)
+            {
+                if (!alphabets[alphabet].Contains(symbol))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static string Shift(string text, int shift, Alphabet alphabet)
+        {
+            var stringBuilder = new StringBuilder(text.Length);
+            foreach (var symbol in text)
+            {
+                int index = alphabets[alphabet].IndexOf(symbol);
+                index = (index + shift) % alphabets[alphabet].Length;
+                stringBuilder.Append(alphabets[alphabet][index]);
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        public static string Encrypt(string text, int key, Alphabet alphabet)
+        {
+            return Shift(text, key, alphabet);
+        }
+
+        public static string Decrypt(string text, int key, Alphabet alphabet)
+        {
+            return Shift(text, -key, alphabet);
+        }
+    }
+}
