1e81fe5d1296eee1ef9d920aec2625451bbc6352
Maksym Dovhan <kerman901@gmail.com>
2020-02-18 14:02:08 +0200

    Add unit tests to TrithemiusCipher

---
+++ solutions/TrithemiusCipher/TrithemiusCipher.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TrithemiusCipher.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TrithemiusCipher.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a9a8dd8d-7cc2-4d97-bb0c-a122767738eb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ solutions/TrithemiusCipher/TrithemiusCipher.Tests/TrithemiusCipherTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using static TrithemiusCipher.Logic.TrithemiusCipher;
+
+namespace TrithemiusCipher.Tests
+{
+    [TestClass]
+    public class TrithemiusCipherTests
+    {
+        [TestMethod]
+        public void TestEncryptAndDecryptLinear()
+        {
+            string inputText = "hello world";
+            string encryptedText = Encrypt(inputText, Logic.Alphabet.English, -50, -3);
+            string decryptedText = Decrypt(encryptedText, Logic.Alphabet.English, -50, -3);
+
+            Assert.AreEqual(inputText, decryptedText);
+        }
+
+        [TestMethod]
+        public void TestEncryptAndDecryptQuadratic()
+        {
+            string inputText = "hello world";
+            string encryptedText = Encrypt(inputText, Logic.Alphabet.English, -50, -3, 2);
+            string decryptedText = Decrypt(encryptedText, Logic.Alphabet.English, -50, -3, 2);
+
+            Assert.AreEqual(inputText, decryptedText);
+        }
+
+        [TestMethod]
+        public void TestEncryptAndDecryptMoto()
+        {
+            string inputText = "hello world";
+            string encryptedText = Encrypt(inputText, Logic.Alphabet.English, "moto");
+            string decryptedText = Decrypt(encryptedText, Logic.Alphabet.English, "moto");
+
+            Assert.AreEqual(inputText, decryptedText);
+        }
+    }
+}
