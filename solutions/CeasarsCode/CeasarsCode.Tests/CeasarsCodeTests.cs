dc5024585f64b8d409f8e4af305ad700dbdc23a1
Maksym Dovhan <kerman901@gmail.com>
2020-02-18 15:15:35 +0200

    Add CeasarsCodeTests

---
+++ solutions/CeasarsCode/CeasarsCode.Tests/CeasarsCodeTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using static CeasarsCode.Logic.CeasarsCode;
+
+namespace CeasarsCode.Tests
+{
+    [TestClass]
+    public class CeasarsCodeTests
+    {
+        private string engAlphabet = " 'abcdefghijklmnopqrstuvwxyz";
+        //private string ukrAlphabet = " 'абвгґдеєжзиіїйклмнопрстуфхцчшщьюя";
+
+        private string GetRandomString(string alphabet)
+        {
+            var rand = new Random();
+            int length = rand.Next(1, 256);
+            var stringBuilder = new StringBuilder(length);
+            for (int i = 0; i < length; ++i)
+            {
+                stringBuilder.Append(alphabet[rand.Next(alphabet.Length)]);
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        private int GetRandomKey(string alphabet)
+        {
+            var rand = new Random();
+            return rand.Next(alphabet.Length);
+        }
+
+        [TestMethod]
+        public void TestEncryptAndDecrytEnglish()
+        {
+            string inputText = "hello world";
+            string encryptedText = Encrypt(inputText, 14 ,Logic.Alphabet.English);
+            string decryptedText = Decrypt(encryptedText, 14, Logic.Alphabet.English);
+
+            Assert.AreNotEqual(inputText, encryptedText);
+            Assert.AreEqual(inputText, decryptedText);
+        }
+
+        [TestMethod]
+        public void TestRandomEncryptAndDecryptEnglish()
+        {
+            int iterations = 100;
+            for (int i = 0; i < iterations; ++i)
+            {
+                var engText = GetRandomString(engAlphabet);
+                var engKey = GetRandomKey(engAlphabet);
+
+                var encryptedEngText = Encrypt(engText, engKey, Logic.Alphabet.English);
+                var decryptedEngText = Decrypt(encryptedEngText, engKey, Logic.Alphabet.English);
+
+                Assert.AreNotEqual(engText, encryptedEngText);
+                Assert.AreEqual(engText, decryptedEngText);
+            }
+        }
+    }
+}
---
+++ solutions/CeasarsCode/CeasarsCode.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CeasarsCode.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CeasarsCode.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("88b0d34c-988e-4347-98ed-6f84a5253878")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
